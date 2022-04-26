15a9934d91ca5cf043fb08c7924b9677e0d31338
xeptao <xeptaowastaken@gmail.com>
2022-04-26 20:57:14 +0300

    feat: func. `traverse_tree`

--- src/script.py
+++ src/script.py
@@ -1,5 +1,26 @@
+from classes.node import Node
+
+tree = Node(0, Node(1, Node(2), Node(3)), Node(4, Node(5), Node(6)))
+
+
 def traverse_tree(node):
-    pass
+    print(node.value)
+
+    if node.right or node.left:
+        if node.right:
+            print(node.right.value)
+
+            if node.right.right or node.right.left:
+                traverse_tree(node.right.right if node.right.right else None)
+                traverse_tree(node.right.left if node.right.left else None)
+
+        if node.left:
+            print(node.left.value)
+
+            if node.left.right or node.left.left:
+                traverse_tree(node.left.right if node.left.right else None)
+                traverse_tree(node.left.left if node.left.left else None)
+
 
 if __name__ == "__main__":
-    traverse_tree()
+    traverse_tree(tree)
